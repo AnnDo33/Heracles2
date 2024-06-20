3eddff6effa63b21b45ac1191066624913732db7
AnnDo33 <ann.dominique.d@gmail.com>
2024-06-20 14:45:55 +0200

    finished step 2

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const Fighter = require("./src/Fighter.js");
+const Weapon = require("./src/Weapon.js");
+const Shield = require("./src/Shield.js");
 
 /** Create Heracles  */
 const heracles = new Fighter("🧔 Heracles", 20, 6);
+const sword = new Weapon("Épée", 10);
+heracles.weapon = sword;
+const shield = new Shield(10);
+heracles.shield = shield;
 
 /** Create the opponent  */
 const boar = new Fighter("🐗 Erymanthian Boar", 25, 12);
@@ -17,11 +23,30 @@ const roundDisplay = (fighter1, fighter2) => {
  * Helper to dispatch fighters between winner and loser
  */
 const score = (fighter1, fighter2) => {
-  return fighter1.isAlive() ? {
-    winner: fighter1,
-    loser: fighter2,
-  } : {
-    winner: fighter2,
-    loser: fighter1
-  };
+  return fighter1.isAlive()
+    ? {
+        winner: fighter1,
+        loser: fighter2,
+      }
+    : {
+        winner: fighter2,
+        loser: fighter1,
+      };
 };
+
+// Combat loop
+let round = 1;
+
+while (heracles.isAlive() && boar.isAlive()) {
+  console.log(`🕛 Round ${round}`);
+  heracles.fight(boar);
+  if (boar.isAlive()) {
+    boar.fight(heracles);
+  }
+  console.log(roundDisplay(heracles, boar));
+  round++;
+}
+
+const finalScore = score(heracles, boar);
+console.log(`🏆 ${finalScore.winner.name} est le vainqueur !`);
+console.log(`💀 ${finalScore.loser.name} est défait.`);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,34 +1,44 @@
 const MAX_LIFE = 100;
 
 class Fighter {
-    constructor(name, strength, dexterity) {
-        this.name = name;
-        this.strength = strength;
-        this.dexterity = dexterity;
-        this.life = MAX_LIFE;
-    }
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+    this.weapon = null; // Nouvelle propriété pour l'arme
+    this.shield = null; // Nouvelle propriété pour le bouclier
+  }
 
+  // Launch a fight
+  fight(defender) {
+    const attackPoints = this.getRandomInt(this.getDamage());
+    const damages = Math.max(attackPoints - defender.getDefense(), 0);
+    defender.life = Math.max(defender.life - damages, 0);
+    console.log(
+      `${this.name} attaque ${defender.name} et inflige ${damages} points de dommage.`
+    );
+  }
 
-    // Launch a fight
-    fight(defender) {
-        const attackPoints = this.getRandomInt(this.strength);
+  // Generate a random value between 1 and max
+  getRandomInt(max) {
+    return 1 + Math.floor(Math.random() * max);
+  }
 
-        const damages = Math.max(attackPoints - defender.dexterity, 0);
+  // Determine if a fighter is still alive
+  isAlive() {
+    return this.life > 0;
+  }
 
-        defender.life = Math.max(defender.life - damages, 0);
-    }
+  // Get the total damage including weapon
+  getDamage() {
+    return this.strength + (this.weapon ? this.weapon.damage : 0);
+  }
 
-
-    // Generate a random value between 1 and max
-    getRandomInt(max) {
-        return 1 + Math.floor(Math.random() * max);
-    }
-
-
-    // Determine if a fighter is still alive
-    isAlive() {
-        return this.life > 0;
-    }
+  // Get the total defense including shield
+  getDefense() {
+    return this.dexterity + (this.shield ? this.shield.protection : 0);
+  }
 }
 
 module.exports = Fighter;
